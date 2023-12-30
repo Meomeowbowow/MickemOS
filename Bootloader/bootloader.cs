8ff65d76d8092efc69d16d5703d41a6954d13cd8
Meomeowbowow <111109798+Meomeowbowow@users.noreply.github.com>
2023-12-30 16:27:54 +1100

    Added Bootloader.cs File

---
+++ Bootloader/bootloader.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using MickemOS.Core;
+
+namespace MickemOS.Bootloader
+{
+    class bootloader
+    {
+        public static void StartBootProccess()
+        {
+            Console.Clear();
+            Console.Write("[  ");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("OK");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("  ] ");
+            Console.Write("Loading Main Kernel...\n");
+            Thread.Sleep(3000);
+            Console.Write("[  ");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("OK");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("  ] ");
+            Console.Write("Main Kernel Loaded\n");
+            Thread.Sleep(1000);
+            Console.Write("[  ");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("OK");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("  ] ");
+            Console.Write("Loading Data...\n");
+            Thread.Sleep(2000);
+            Console.Write("[  ");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("OK");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("  ] ");
+            Console.Write("Initializing 'CommandHandler.cs'...\n");
+            Thread.Sleep(2000);
+            Console.Write("[  ");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("OK");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("  ] ");
+            Console.Write("Loading All MickemOS Files...\n");
+            Thread.Sleep(2000);
+            Console.Write("[  ");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("OK");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("  ] ");
+            Console.Write("Starting file 'Main.cs'...\n");
+            Thread.Sleep(1000);
+            Console.Write("Welcome to ");
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.Write("MickemOS");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("!");
+            Thread.Sleep(2000);
+            MickemOS.Core.Main.AfterKernel();
+        }
+    }
+}
--- Kernel.cs
+++ Kernel.cs
@@ -3,71 +3,27 @@ using System.Threading;
 using System.IO;
 using Sys = Cosmos.System;
 using MickemOS.Core;
+using MickemOS.Bootloader;
 
 namespace MickemOS.Kernel
 {
     public class Kernel : Sys.Kernel
     {
-        public static string kernel_ver = "1.4";
-        public static string os_build = "1305";
+        public static string kernel_ver = "1.6";
+        public static string os_build = "1406";
 
         protected override void BeforeRun()
         {
-            Console.Write("[  ");
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.Write("OK");
-            Console.ForegroundColor = ConsoleColor.White;
-            Console.Write("  ] ");
-            Console.Write("Loading Main Kernel...\n");
-            Thread.Sleep(3000);
-            Console.Write("[  ");
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.Write("OK");
-            Console.ForegroundColor = ConsoleColor.White;
-            Console.Write("  ] ");
-            Console.Write("Main Kernel Loaded\n");
+            Console.Write("Starting MickemOS Bootloader");
             Thread.Sleep(1000);
-            Console.Write("[  ");
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.Write("OK");
-            Console.ForegroundColor = ConsoleColor.White;
-            Console.Write("  ] ");
-            Console.Write("Loading Data...\n");
-            Thread.Sleep(2000);
-            Console.Write("[  ");
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.Write("OK");
-            Console.ForegroundColor = ConsoleColor.White;
-            Console.Write("  ] ");
-            Console.Write("Initializing 'CommandHandler.cs'...\n");
-            Thread.Sleep(2000);
-            Console.Write("[  ");
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.Write("OK");
-            Console.ForegroundColor = ConsoleColor.White;
-            Console.Write("  ] ");
-            Console.Write("Loading All MickemOS Files...\n");
-            Thread.Sleep(2000);
-            Console.Write("[  ");
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.Write("OK");
-            Console.ForegroundColor = ConsoleColor.White;
-            Console.Write("  ] ");
-            Console.Write("Starting file 'Main.cs'...\n");
-            Thread.Sleep(1000);
-            Console.Write("Welcome to ");
-            Console.ForegroundColor = ConsoleColor.Magenta;
-            Console.Write("MickemOS");
-            Console.ForegroundColor = ConsoleColor.White;
-            Console.Write("!");
-            Thread.Sleep(2000);
-            MickemOS.Core.Main.AfterKernel();
+            MickemOS.Bootloader.bootloader.StartBootProccess();
+
 
         }
 
         protected override void Run()
         {
-            
+            //NOT USING THIS FUNCTION - USING MAIN.CS FILE
         }
     }
 }
